a80df892770fed4ba19d9be98aa61be2d77f0b31
jeffpippinsgames <jeffpippinsgames@gmail.com>
2021-03-18 15:13:08 -0700

    Working

---
+++ src/harware_defines.h
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+
+#define TBI_SERIALBAUD 115200
+
+#define TBI_XSTEPPIN 1
+#define TBI_ZSTEPPIN 4
+#define TBI_XDIRPIN 2
+#define TBI_ZDIRPIN  5
+#define TBI_XENABLEPIN 3
+#define TBI_ZENABLEPIN 6
+#define TBI_XLIMITPIN 7
+#define TBI_ZLIMITPIN 8
+
+#define TBI_XMAXSPEED 4000 //Steps Per Sec
+#define TBI_XPULLINSPEED 200 //Initial Speed Without Accel. 
+#define TBI_XPULLOUTSPEED 200 //Initial Speed Without Accel. 
+#define TBI_XMAXACCEL 12000 //Steps Per Sec ^2
+#define TBI_XDRIVERPINPOLARITY LOW // The Stepper Motors Active Step and Dir Polarity
+#define TBI_XINVERTDIRPIN false //Inverts the Dir Pin
+#define TBI_XJOYSTICKSPEED 1200 //Default Joystick Speed
+
+#define TBI_ZMAXSPEED 4000 //Steps Per Sec
+#define TBI_ZPULLINSPEED 200 //Initial Speed Without Accel. 
+#define TBI_ZPULLOUTSPEED 200 //Initial Speed Without Accel. 
+#define TBI_ZMAXACCEL 12000 //Steps Per Sec ^2
+#define TBI_ZDRIVERPINPOLARITY LOW // The Stepper Motors Active Step and Dir Polarity
+#define TBI_ZINVERTDIRPIN false //Inverts the Dir Pin
+#define TBI_ZJOYSTICKSPEED 1200 //Default Joystick Speed
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
 #include <Arduino.h>
+#include <TeensyStep.h>
+#include <PID_v1.h>
+#include "harware_defines.h"
+
+
+Stepper x_motor(TBI_XSTEPPIN, TBI_XDIRPIN), z_motor(TBI_ZSTEPPIN, TBI_ZDIRPIN);
+RotateControl x_controller;
+RotateControl Z_controller;
+
+
+
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(TBI_SERIALBAUD);
+  //X Motor Setup
+  x_motor.setMaxSpeed(TBI_XMAXSPEED);
+  x_motor.setPullInSpeed(TBI_XPULLINSPEED);
+  x_motor.setPullInOutSpeed(TBI_XPULLOUTSPEED);
+  x_motor.setAcceleration(TBI_XMAXACCEL);
+  x_motor.setStepPinPolarity(TBI_XDRIVERPINPOLARITY);
+  x_motor.setInverseRotation(TBI_XINVERTDIRPIN);
+  //Z Motor Setup
+  z_motor.setMaxSpeed(TBI_ZMAXSPEED);
+  z_motor.setPullInSpeed(TBI_ZPULLINSPEED);
+  z_motor.setPullInOutSpeed(TBI_ZPULLOUTSPEED);
+  z_motor.setAcceleration(TBI_ZMAXACCEL);
+  z_motor.setStepPinPolarity(TBI_ZDRIVERPINPOLARITY);
+  z_motor.setInverseRotation(TBI_ZINVERTDIRPIN);
+  //
+  x_motor.setPosition(0);
+  z_motor.setPosition(0);
+
+  
+
 }
 
 void loop() {
