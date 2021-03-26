838918e741a02d8563f4264535261968843d5de8
jeffpippinsgames <jeffpippinsgames@gmail.com>
2021-03-26 14:18:59 -0700

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,7 @@
 //--------------------------------------------------------------------------------------------------------------------
 //Defines
 #define __DEBUG
+#define BUFFER_SIZE 8
 //--------------------------------------------------------------------------------------------------------------------
 
 
@@ -34,7 +35,7 @@
 //--------------------------------------------------------------------------------------------------------------------
 //Enumerated Types
 enum PIDState_t{TBI_PID_STATE_OFF, TBI_PID_STATE_ON};
-enum SerialCommand_t{TBI_CMD_DELIMITER_CR = 0x0D, TBI_CMD_DELMIITER_LF = 0x0A, TBI_CMD_SEND_WATCHDOG = 0x01, TBI_CMD_STOP_MOVEMENT = 0x02, TBI_CMD_JOG_UP = 0x03, TBI_CMD_JOG_DOWN = 0x04, TBI_JOG_LEFT = 0x05, TBI_JOG_RIGHT = 0x06, TBI_SET_X_ERROR = 0x07,
+enum SerialCommand_t{ TBI_CMD_DELIMITER_CR = 0x0D, TBI_CMD_DELMIITER_LF = 0x0A, TBI_CMD_SEND_WATCHDOG = 0x01, TBI_CMD_STOP_MOVEMENT = 0x02, TBI_CMD_JOG_UP = 0x03, TBI_CMD_JOG_DOWN = 0x04, TBI_CMD_JOG_LEFT = 0x05, TBI_CMD_JOG_RIGHT = 0x06, TBI_SET_X_ERROR = 0x07,
                       TBI_SET_Z_ERROR = 0x08, TBI_CMD_SEND_X_ERROR = 0x09, TBI_CMD_SEND_Z_ERROR = 0x0B, TBI_CMD_SEND_X_LIMIT_TRIPPED = 0x0C, TBI_CMD_SEND_Z_LIMIT_TRIPPED = 0x0E, TBI_CMD_SEND_X_POSITIION = 0x0F, TBI_CMD_SEND_Z_POSITION = 0x10, 
                       TBI_CMD_SET_X_POSITION = 0x11, TBI_CMD_SET_Z_POSITION = 0x12, TBI_CMD_SEND_X_LIMIT_STATUS = 0x13, TBI_CMD_SEND_Z_LIMIT_STATUS = 0x14, TBI_CMD_SET_KP_XCONTROL = 0x15, TBI_CMD_SET_KD_XCONTROL = 0x16, TBI_CMD_SET_KI_XCONTROL = 0x17,
                       TBI_CMD_SEND_KP_XCONTROL = 0x18, TBI_CMD_SEND_KD_XCONTROL = 0x19, TBI_CMD_SEND_KI_XCONTROL = 0x20, TBI_CMD_PID_ON = 0x21, TBI_CMD_PID_OFF = 0x22, TBI_CMD_SEND_STATUS = 0x23, TBI_CMD_HOME_X = 0x24, TBI_CMD_HOME_Z = 0x25, 
@@ -46,8 +47,9 @@ Serial Command Key
 The Teensy Appends the Standard Serial Object. See Teensy for more detaills. 
 
 The Serial Commands Are Shared WIth The Host Application in order to facilitate control of the PID Based Motor Control.
-The Command Control Schema will be one leading byte followed by any data appropriate to the command.
-The Delimiter will be LF Char 
+The Command Control Schema will be one leading byte which represents the Command Followed By Status Bytes and or raw data.
+The Schema is a fixed data buffer size schema of max size BUFFER_SIZE. It is up to the application to appropriatly manage the 
+buffer and serial send and recieve management.
 
 TBI_CMD_DELIMITER_CR: Command Delimiter
 TBI_CMD_DELMIITER_LF: Command Delimiter
@@ -55,8 +57,8 @@ TBI_CMD_SEND_WATCHDOG: Instructs the control to send a TBI_STATUS_WATCHDOG_OK si
 TBI_CMD_STOP_MOVEMENT: Stops all Motor Movement
 TBI_CMD_JOG_UP: Makes the controller jog up
 TBI_CMD_JOG_DOWN: Makes the controller jog down
-TBI_JOG_LEFT: Makes the controller jog left
-TBI_JOG_RIGHT: Makes the controller jog right.
+TBI_CMD_JOG_LEFT: Makes the controller jog left
+TBI_CMD_JOG_RIGHT: Makes the controller jog right.
 TBI_SET_X_ERROR: Sets the controller PID error value for the X axis
 TBI_SET_Z_ERROR: Sets the controller PID error value for the Z axis
 TBI_CMD_SEND_X_ERROR: Instructs the controller to send the current PID X axis error value
@@ -89,13 +91,15 @@ TBI_CMD_SEND_KI_ZCONTROL: Sends the PID KI for the Z axis PID controller
 */
 enum ControlStatus_t {TBI_STATUS_IDLE = 0x00, TBI_STATUS_MOVING = 0x01, TBI_STATUS_X_LIMIT_TRIPPED = 0x02, TBI_STATUS_Z_LIMIT_TRIPPED = 0x03, TBI_STATUS_LIMIT_TRIPPED = 0x04, TBI_STATUS_GENERAL_ERROR = 0x05, TBI_STATUS_WATCHDOG_OK = 0x06, TBI_STATUS_X_LIMIT_OK = 0x07, TBI_STATUS_Z_LIMIT_OK = 0x08};
 //--------------------------------------------------------------------------------------------------------------------
-
+enum MotionStatus_t {TBI_MOTION_STATUS_IDLE = 0x00, TBI_MOTION_STATUS_MOVING_PID = 0x01, TBI_MOTION_STATUS_JOGGING = 0x02 };
 
 
 
 //--------------------------------------------------------------------------------------------------------------------
 //Global Variables
 PIDState_t pid_state = TBI_PID_STATE_OFF;
+MotionStatus_t motion_status = TBI_MOTION_STATUS_IDLE;
+
 
 Stepper x_motor(TBI_XSTEPPIN, TBI_XDIRPIN);
 Stepper z_motor(TBI_ZSTEPPIN, TBI_ZDIRPIN);
@@ -109,6 +113,12 @@ PID xPID(&xPID_input, &xPID_output, &xPID_setpoint, x_Kp, x_Ki, x_Kd, P_ON_E, DI
 double zPID_setpoint, zPID_input, zPID_output;
 double z_Kp=2, z_Ki=5, z_Kd=1;
 PID zPID(&zPID_input, &zPID_output, &zPID_setpoint, z_Kp, z_Ki, z_Kd, P_ON_E, DIRECT);
+
+
+
+byte incomming_serial_buffer[BUFFER_SIZE];
+byte outgoing_serial_buffer[BUFFER_SIZE];
+
 //--------------------------------------------------------------------------------------------------------------------
 
 
@@ -153,6 +163,7 @@ void setup()
   while(Serial.available()) Serial.read();
   //Turn on the Teensy LED To Indicate the System is Setup and Communicating.
   digitalWrite(LED_BUILTIN, HIGH);
+  
   Serial.println("TBIVision-TeensyMotionControl Inititalzed and Connected. Ready For Commands");
 }
 //--------------------------------------------------------------------------------------------------------------------
@@ -164,14 +175,18 @@ void setup()
 void loop()
  {
 
+
+
+
   //If there is a Serial Connection Then Process The Commands.
   if(Serial.dtr() && Serial)
   {
-    //Check Serial Data
-    while(Serial.available())
-    {
-      
-    }
+    
+    clearIncommingSerialBuffer(); //Clear Serial Buffers
+    clearOutgoingSerialBuffer(); //Clear Serial Buffers
+    digitalWrite(LED_BUILTIN, HIGH); //Make Sure LED Is On. Everything is Fine.
+    //Get The Next Command
+    processIncommingSerialCommand(incomming_serial_buffer);
 
     //Process Command
 
@@ -182,19 +197,88 @@ void loop()
   }
   else //Bad JooJoo. We lost the Serial Connection. TURN IT OFF
   {
-   pid_state = TBI_PID_STATE_OFF;
-   if(x_controller.isRunning()) x_controller.stop();
-   if(z_controller.isRunning()) z_controller.stop();
+    digitalWrite(LED_BUILTIN, LOW); // Turn Off LED. Bad JooJoo....
+    pid_state = TBI_PID_STATE_OFF;
+    if(x_controller.isRunning()) x_controller.stop();
+    if(z_controller.isRunning()) z_controller.stop();
   }
 }
 //--------------------------------------------------------------------------------------------------------------------
 
 
-
-
 //--------------------------------------------------------------------------------------------------------------------
 //Command Processing Functions.
 //--------------------------------------------------------------------------------------------------------------------
+void processIncommingSerialCommand(byte* _data)
+{
+  if(!_data) return; 
+  if(Serial.available() == BUFFER_SIZE)
+  {
+      Serial.readBytes((char*)_data, BUFFER_SIZE);
+      switch(_data[0])
+      {
+        case TBI_CMD_STOP_MOVEMENT:
+          processStopMovementCmd();
+          break;
+        case TBI_CMD_JOG_UP:
+          processJogUpCmd();
+          break;
+        case TBI_CMD_JOG_DOWN:
+          processJogDownCmd();
+          break;
+        case TBI_CMD_JOG_LEFT:
+          processJogLeftCmd();
+          break;
+        case TBI_CMD_JOG_RIGHT:
+          processJogRightCmd();
+          break;
+        
+      }
+
+
+  
+  }
+}
+
+void processJogUpCmd()
+{
+  if(motion_status != TBI_MOTION_STATUS_IDLE) return;
+  motion_status = TBI_MOTION_STATUS_JOGGING;
+  z_controller.overrideSpeed(.5);
+  z_controller.rotateAsync(z_motor);
+}
+
+void processJogDownCmd()
+{
+  if(motion_status != TBI_MOTION_STATUS_IDLE) return;
+  motion_status = TBI_MOTION_STATUS_JOGGING;
+  z_controller.overrideSpeed(-.5);
+  z_controller.rotateAsync(z_motor);
+}
+
+void processJogLeftCmd()
+{
+  if(motion_status != TBI_MOTION_STATUS_IDLE) return;
+  motion_status = TBI_MOTION_STATUS_JOGGING;
+  x_controller.overrideSpeed(.5);
+  x_controller.rotateAsync(x_motor);
+}
+
+void processJogRightCmd()
+{
+  if(motion_status != TBI_MOTION_STATUS_IDLE) return;
+  motion_status = TBI_MOTION_STATUS_JOGGING;
+  x_controller.overrideSpeed(-.5);
+  x_controller.rotateAsync(x_motor);  
+}
+
+void processStopMovementCmd()
+{
+  motion_status = TBI_MOTION_STATUS_IDLE;
+  x_controller.stop();
+  z_controller.stop();
+}
+
 
 
 
@@ -222,3 +306,19 @@ long parseLongData(byte *_data)
 }
 //--------------------------------------------------------------------------------------------------------------------
 
+
+
+
+//--------------------------------------------------------------------------------------------------------------------
+//Misc Functions
+void clearIncommingSerialBuffer()
+{
+    for(int i = 0; i < BUFFER_SIZE; ++i) incomming_serial_buffer[i] = 0x00;
+}
+
+void clearOutgoingSerialBuffer()
+{
+  for(int i = 0; i < BUFFER_SIZE; ++i) outgoing_serial_buffer[i] = 0x00;
+}
+
+//--------------------------------------------------------------------------------------------------------------------
