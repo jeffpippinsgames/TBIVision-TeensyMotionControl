e8436154ce9b7e5472c64a1f8698ed55d5ffa58f
Jeff Pippins <jeffpippinsgames@gmail.comcd>
2021-04-05 16:04:19 -0700

    Working Copy

--- src/tbicommandmanager.cpp
+++ src/tbicommandmanager.cpp
@@ -8,17 +8,21 @@ TBICommandManager::TBICommandManager()
 void TBICommandManager::processCommandData(RotateControl* _x_control, RotateControl* _z_control, Stepper* _x_motor, Stepper* _z_motor, TBILimitSwitches* _limitswitches, TBIControlStatusContainer* _control_status)
 {
   
-  if(Serial.available() == TBI_COMMAND_BUFFER_SIZE)
+  if(Serial.available())
   {
-      Serial.readBytes((char*)incomming_command_buffer, TBI_COMMAND_BUFFER_SIZE);
-      switch(incomming_command_buffer[0])
+
+      byte _cmd;
+      _cmd = (byte)Serial.read();
+      switch(_cmd)
       {
         //----------------------------------------------------------
         case TBI_CMD_STOP_MOVEMENT:
           this->doStopMovement(_x_control, _z_control, _control_status);
+          Serial.flush();
           break;
         //----------------------------------------------------------
         case TBI_CMD_JOG_UP:
+        /*
           if(_limitswitches->GetZPlusLimitSwitchState() == TBI_LIMIT_SWITCH_STATE_TRIPPED)
           {
             this->doStopMovement(_x_control, _z_control, _control_status);
@@ -27,7 +31,11 @@ void TBICommandManager::processCommandData(RotateControl* _x_control, RotateCont
           {
             this->doMotorRotation(_z_control, _z_motor, _control_status, _limitswitches, .5);
           }
+          */
+         
+         this->doMotorRotation(_z_control, _z_motor, _control_status, _limitswitches, .5);
           break;
+          /*
         //----------------------------------------------------------
         case TBI_CMD_JOG_DOWN:
           if(_limitswitches->GetZMinusLimitSwitchState() == TBI_LIMIT_SWITCH_STATE_TRIPPED)
@@ -60,7 +68,7 @@ void TBICommandManager::processCommandData(RotateControl* _x_control, RotateCont
           {
             this->doMotorRotation(_x_control, _x_motor, _control_status, _limitswitches, -.5);
           }
-          break; 
+          break; */
         //---------------------------------------------------------- 
       }
   }
@@ -73,10 +81,9 @@ void TBICommandManager::processCommandData(RotateControl* _x_control, RotateCont
 void TBICommandManager::doMotorRotation(RotateControl *_rotcontroller, Stepper* _motor, TBIControlStatusContainer *_control_status, TBILimitSwitches* _limitswitches, float _speed_fraction)
 {
     if(_control_status->GetMotionStatus() != TBI_MOTION_STATUS_IDLE) return;
-
-  _control_status->SetMotionStatus(TBI_MOTION_STATUS_JOGGING);
   _rotcontroller->overrideSpeed(.5);
   _rotcontroller->rotateAsync(*_motor);
+  _control_status->SetMotionStatus(TBI_MOTION_STATUS_JOGGING);
 }
 //--------------------------------------------------------------------------------------------------------------------
 void TBICommandManager::doStopMovement(RotateControl *_x_control, RotateControl* _z_control, TBIControlStatusContainer* _control_status)
